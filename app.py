150a869859f8b2a70f1187b03498b6b1353b6549
mircea <mircea.rautoiu@gmail.com>
2024-04-08 18:29:35 +0300

    init project

---
+++ app.py
@@ -0,0 +1,39 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="NetChill API",
+    description="API for NetChill",
+    version="0.1.0",
+    swagger_ui_parameters={"defaultModelsExpandDepth": -1},
+)
+
+origins = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:5173',
+]
+
+# Set up CORS middleware to allow cross-origin requests from the defined origins.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows access from specified origins
+    allow_credentials=True,  # Allows cookies to be included in cross-origin HTTP requests
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  # Specify which method are allowed
+    allow_headers=["X-Requested-With", "Content-Type", "Accept", "Origin", "Authorization"],  # Specific headers allowed
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("app:app",
+                host="localhost",
+                port=8000,
+                reload=True,
+                ssl_keyfile="C:/Windows/System32/localhost+2-key.pem",
+                ssl_certfile="C:/Windows/System32/localhost+2.pem")
